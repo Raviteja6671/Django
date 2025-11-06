78bac1beb70fc9643cd91ab4cea152b356a7c4ba
raviteja6671 <ravitejaraviteja6671@gmail.com>
2025-11-06 20:31:46 +0530

    host is add

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9fosf6)!x42c8b!@+#r=)@0pj)!v-q^02sf-%m^*7m8fk#h_*2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://django-app-wykh.onrender.com"]
 
 
 # Application definition
